117c1012253e4f601ce3149901a841e06a3960f7
Corey Huffman <cohu8717@colorado.edu>
2023-09-18 23:08:04 -0600

    set up for 2 bugs

--- ws/main.cpp
+++ ws/main.cpp
@@ -5,7 +5,8 @@
 #include "hw/HW2.h"
 
 // Include any custom headers you created in your workspace
-#include "MyBugAlgorithm.h"
+#include "MyBug1Algorithm.h"
+#include "MyBug2Algorithm.h"
 
 using namespace amp;
 
@@ -14,12 +15,11 @@ int main(int argc, char** argv) {
     /*    Randomly generate the problem     */ 
 
     // Use WO1 from Exercise 2
-    Problem2D problem = HW2::getWorkspace1();
+    Problem2D problem1 = HW2::getWorkspace1();
 
     // Use WO1 from Exercise 2
-    /*
-    Problem2D problem = HW2::getWorkspace2();
-    */
+    Problem2D problem2 = HW2::getWorkspace2();
+
 
     // Make a random environment spec, edit properties about it such as the number of obstacles
     /*
@@ -34,19 +34,57 @@ int main(int argc, char** argv) {
     */
 
     // Declare your algorithm object 
-    MyBugAlgorithm algo;
+    MyBug1Algorithm algo1;
+    MyBug2Algorithm algo2;
     
+    // Bug 1
     {
         // Call your algorithm on the problem
-        amp::Path2D path = algo.plan(problem);
+        amp::Path2D path_1_w1 = algo1.plan(problem1);
 
         // Check your path to make sure that it does not collide with the environment 
-        bool success = HW2::check(path, problem);
+        bool success = HW2::check(path_1_w1, problem1);
 
         LOG("Found valid solution to workspace 1: " << (success ? "Yes!" : "No :("));
 
         // Visualize the path and environment
-        Visualizer::makeFigure(problem, path);
+        Visualizer::makeFigure(problem1, path_1_w1);
+
+        // Call your algorithm on the problem
+        amp::Path2D path_1_w2 = algo1.plan(problem2);
+
+        // Check your path to make sure that it does not collide with the environment 
+        success = HW2::check(path_1_w2, problem2);
+
+        LOG("Found valid solution to workspace 2: " << (success ? "Yes!" : "No :("));
+
+        // Visualize the path and environment
+        Visualizer::makeFigure(problem2, path_1_w2);
+    }
+
+    // Bug 2
+    {
+        // Call your algorithm on the problem
+        amp::Path2D path_2_w1 = algo2.plan(problem1);
+
+        // Check your path to make sure that it does not collide with the environment 
+        bool success = HW2::check(path_2_w1, problem1);
+
+        LOG("Found valid solution to workspace 2: " << (success ? "Yes!" : "No :("));
+
+        // Visualize the path and environment
+        Visualizer::makeFigure(problem1, path_2_w1);
+
+        // Call your algorithm on the problem
+        amp::Path2D path_2_w2 = algo2.plan(problem2);
+
+        // Check your path to make sure that it does not collide with the environment 
+        success = HW2::check(path_2_w2, problem2);
+
+        LOG("Found valid solution to workspace 2: " << (success ? "Yes!" : "No :("));
+
+        // Visualize the path and environment
+        Visualizer::makeFigure(problem2, path_2_w2);
     }
 
     // Let's get crazy and generate a random environment and test your algorithm
@@ -54,7 +92,7 @@ int main(int argc, char** argv) {
         amp::Path2D path; // Make empty path, problem, and collision points, as they will be created by generateAndCheck()
         amp::Problem2D random_prob; 
         std::vector<Eigen::Vector2d> collision_points;
-        bool random_trial_success = HW2::generateAndCheck(algo, path, random_prob, collision_points);
+        bool random_trial_success = HW2::generateAndCheck(algo2, path, random_prob, collision_points);
         LOG("Found valid solution in random environment: " << (random_trial_success ? "Yes!" : "No :("));
 
         LOG("path length: " << path.length());
@@ -65,7 +103,7 @@ int main(int argc, char** argv) {
 
     Visualizer::showFigures();
 
-    HW2::grade(algo, "cohu8717@colorado.edu", argc, argv);
+    HW2::grade(algo2, "cohu8717@colorado.edu", argc, argv);
 
     return 0;
 }
