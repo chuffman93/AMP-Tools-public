0d9cf53f9174c2c54b3cc9e5b14f07988b8c03fb
Corey Huffman <cohu8717@colorado.edu>
2023-09-19 23:21:21 -0600

    added collision code and towards goal

--- ws/MyBug1Algorithm.cpp
+++ ws/MyBug1Algorithm.cpp
@@ -1,14 +1,131 @@
 #include "MyBug1Algorithm.h"
 
 // Implement your methods in the `.cpp` file, for example:
-amp::Path2D MyBug1Algorithm::plan(const amp::Problem2D& problem) {
-
+amp::Path2D MyBug1Algorithm::plan(const amp::Problem2D& problem) 
+{
     // Your algorithm solves the problem and generates a path. Here is a hard-coded to path for now...
     amp::Path2D path;
+    amp::Path2D dirPathToGoal;
+    
+    int numObs = problem.obstacles.size();
+    vector<amp::Path2D> obsPaths = MyBug1Algorithm::obsPaths(problem);
+
+    bool reached = false;
     path.waypoints.push_back(problem.q_init);
-    path.waypoints.push_back(Eigen::Vector2d(1.0, 5.0));
-    path.waypoints.push_back(Eigen::Vector2d(3.0, 9.0));
-    path.waypoints.push_back(problem.q_goal);
+    Eigen::Vector2d qG = problem.q_goal;
+    Eigen::Vector2d qL = problem.q_init;
+    while(!reached)
+    {
+        // Write to goal function
+        if(abs(qL[0] - qG[0]) > MyBug1Algorithm::eps){
+            qL[0] += MyBug1Algorithm::step;
+        }
+        if(abs(qL[1] - qG[1]) > MyBug1Algorithm::eps)
+        {
+            qL[1] += MyBug1Algorithm::step;
+        }
+
+        // Write collision check function 
+        for(int i = 0 ; i < numObs; i++)
+        {
+            vector<Eigen::Vector2d> objwaypoints = obsPaths[i].waypoints;
+            for(int i = 0; i < objwaypoints.size(); i++)
+            {
+                if(((abs(qL[0] - objwaypoints[i][0]) < MyBug1Algorithm::eps) && (abs(qL[1] - objwaypoints[i][1]) < MyBug1Algorithm::eps)))
+                {
+                    cout << "OBJECT HIT\n";
+                }
+            }
+        }
+
+        if(((abs(qL[0] - qG[0]) < MyBug1Algorithm::eps) && (abs(qL[1] - qG[1]) < MyBug1Algorithm::eps)))
+        {
+            reached = true;
+        }
+        path.waypoints.push_back(qL);
+    } 
 
     return path;
+}
+
+amp::Path2D MyBug1Algorithm::lines(Eigen::Vector2d ptA, Eigen::Vector2d ptB) 
+{
+
+    amp::Path2D path;
+    Eigen::Vector2d start = ptA;
+    Eigen::Vector2d goal = ptB;
+    bool reached = false;
+    path.waypoints.push_back(start);
+    Eigen::Vector2d qL = start;
+    while(!reached)
+    {
+
+        bool negXmove = qL[0] > goal[0];
+        bool negYmove = qL[1] > goal[1];
+        if(abs(qL[0] - goal[0]) > MyBug1Algorithm::eps){
+            if(!negXmove)
+            {
+                qL[0] += MyBug1Algorithm::step;
+            }
+            else
+            {
+                qL[0] -= MyBug1Algorithm::step;
+            }
+        }
+
+        if(abs(qL[1] - goal[1]) > MyBug1Algorithm::eps)
+        {
+            if(!negYmove)
+            {
+                qL[1] += MyBug1Algorithm::step;
+            }
+            else
+            {
+                qL[1] -= MyBug1Algorithm::step;
+            }
+        }
+
+        if(((abs(qL[0] - goal[0]) < MyBug1Algorithm::eps) && (abs(qL[1] - goal[1]) < MyBug1Algorithm::eps)))
+        {
+            reached = true;
+        }
+        path.waypoints.push_back(qL);
+    } 
+    return path;
+}
+
+vector<amp::Path2D> MyBug1Algorithm::obsPaths(const amp::Problem2D& problem)
+{
+    int numObs = problem.obstacles.size();
+    vector<amp::Path2D> paths;
+    vector<Eigen::Vector2d> verts;
+    vector<Eigen::Vector2d> tmp;
+    for(int i = 0; i < numObs; i++)
+    {
+        if(MyBug1Algorithm::turn == "left")
+        {
+            verts = problem.obstacles[i].verticesCCW();
+        }
+        else
+        {
+            verts = problem.obstacles[i].verticesCW();
+        }
+        int numVerts = verts.size();
+        amp::Path2D tmpPath;
+        for(int i = 0; i < numVerts-1; i++)
+        {
+            tmp = MyBug1Algorithm::lines(verts[i],verts[i+1]).waypoints;
+            for(int i = 0; i < tmp.size(); i++)
+            {
+                tmpPath.waypoints.push_back(tmp[i]);
+            }
+        }
+        tmp = MyBug1Algorithm::lines(verts[numVerts-1],verts[0]).waypoints;
+        for(int i = 0; i < tmp.size(); i++)
+        {
+            tmpPath.waypoints.push_back(tmp[i]);
+        }
+        paths.push_back(tmpPath);
+    }
+    return paths;
 }
--- ws/MyBug1Algorithm.h
+++ ws/MyBug1Algorithm.h
@@ -2,6 +2,7 @@
 
 #include "AMPCore.h"
 #include "hw/HW2.h"
+using namespace std;
 
 /// @brief Declare your bug algorithm class here. Note this class derives the bug algorithm class declared in HW2.h
 class MyBug1Algorithm : public amp::BugAlgorithm {
@@ -10,7 +11,12 @@ class MyBug1Algorithm : public amp::BugAlgorithm {
         virtual amp::Path2D plan(const amp::Problem2D& problem) override;
 
         // Add any other methods here...
+        double step = 0.01;
+        double eps = 0.0001;
+        string turn = "left";
     
     private:
         // Add any member variables here...
+        amp::Path2D lines(Eigen::Vector2d ptA, Eigen::Vector2d ptB);
+        vector<amp::Path2D> obsPaths(const amp::Problem2D& problem);
 };
