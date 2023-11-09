585b067944e8ba0f2f1a40e05076dda2a76b7199
Corey Huffman <cohu8717@colorado.edu>
2023-11-08 17:05:19 -0700

    some minor changes

--- ws/hw8/myCentralizedMultiAgentRRT.h
+++ ws/hw8/myCentralizedMultiAgentRRT.h
@@ -22,9 +22,74 @@ class myCentralizedMultiAgentRRT : public CentralizedMultiAgentRRT{
 
             return ret;
         }
+        
+        void setSampleSize(int a)
+        {
+            n = a;
+        }
+        int getSampleSize()
+        {
+            return n;
+        }
+
+        void setPtRadius(double a)
+        {
+            r = a;
+        }
+
+        double getPtRadius()
+        {
+            return r;
+        }
+
+        void setEps(double a)
+        {
+            eps = a;
+        }
+
+        double getEps()
+        {
+            return eps;
+        }
+
+        void setPGoal(double a)
+        {
+            p_goal = a;
+        }
+
+        double getPGoal()
+        {
+            return p_goal;
+        }
+
+        void setDecPercision(int a)
+        {
+            dec_per = a;
+        }
+
+        void setSmoothing(bool a)
+        {
+            smooth = a;
+        }
+        long getTime()
+        {
+            return time;
+        }
+
+        bool getValid()
+        {
+            return valid_path;
+        }
 
         virtual ~myCentralizedMultiAgentRRT() {}
 
     private:
+        int n;
+        double r;
+        double eps;
+        double p_goal;
+        int dec_per;
+        long time;
+        bool valid_path;
         myUtils myCMA;
 };
